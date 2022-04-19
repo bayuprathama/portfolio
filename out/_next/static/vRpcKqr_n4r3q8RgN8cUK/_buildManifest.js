10b5e7e81250f6b61a501a41729ca2652c5bedf8
Bayu Prathama <dev.bayuprathama@gmail.com>
2022-04-19 20:33:50 +0800

    add out folder for static deployment

---
+++ out/_next/static/vRpcKqr_n4r3q8RgN8cUK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/47746702592562da.css","static/chunks/pages/index-e7fd890808e89375.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
