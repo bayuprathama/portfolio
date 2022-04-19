c62dd6038c78cb1a9e6713e22faef60200191460
Bayu Prathama <dev.bayuprathama@gmail.com>
2022-04-19 21:03:11 +0800

    update content and styling

---
+++ _next/static/cIGIXowkUfYU7YrOWT2Y3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/47746702592562da.css","static/chunks/pages/index-30873722ef5b6320.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
